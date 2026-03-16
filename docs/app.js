ff8f9fcb9576b4c8044c3ea3e3b4f9841c16f98c
manalk14322-max <manalk14322@gmail.com>
2026-03-16 18:05:23 +0500

    Update iPhone cover images and refresh cache

--- docs/app.js
+++ docs/app.js
@@ -11,7 +11,7 @@
   const KEYS = {
     cart: "twm_cart_modern_v1",
     orders: "twm_orders_modern_v1",
-    productsCache: "twm_products_cache_v7",
+    productsCache: "twm_products_cache_v8",
   };
 
   const els = {
